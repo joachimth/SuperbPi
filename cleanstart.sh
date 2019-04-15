25299e20b5ec346f4a72a4ce4409ef8926b58eb5
joachimth <46874512+joachimth@users.noreply.github.com>
2019-04-15 21:25:38 +0200

    Update cleanstart.sh

--- cleanstart.sh
+++ cleanstart.sh
@@ -3,16 +3,34 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install -y apache2 php7.0 php7.0-mysql php7.0-bcmath mariadb-server libmariadbclient-dev libmariadbclient-dev-compat
+sudo apt install -y apache2 php7.0 php7.0-mysql php7.0-bcmath
+sudo apt install -y mariadb-server libmariadbclient-dev libmariadbclient-dev-compat
 
 sudo apt-get update
-sudo apt-get install -y python python-all-dev python-pip build-essential python-setuptools
+sudo apt-get install -y python3.6
+sudo apt-get install -y python python-all-dev python-setuptools
+sudo apt-get install -y python-pip
 
-sudo pip install pylibftdi
+sudo apt-get install -y build-essential cmake doxygen libusb-1.0-devel
+
+sudo apt-get install -y libconfuse-dev
+sudo apt-get install -y swig python-dev
+sudo apt-get install -y libboost-all-dev
+
+mkdir libftdi
+cd libftdi
+git clone git://developer.intra2net.com/libftdi
+
+cd ~/libftdi
+mkdir build
+cd ~/libftdi/build
+cmake  -DCMAKE_INSTALL_PREFIX="/usr" ../
+make
+sudo make install
 
 cd ~/SuperbPi
-#make
-#sudo make install
+make
+sudo make install
 
 sudo cp -r html /var/www/
 sudo rm /var/www/html/index.html
@@ -21,15 +39,16 @@ sudo chmod -R 0755 /var/www/html
 
 sudo chown -R root:root ~/SuperbPi
 sudo chown -R 0755 ~/SuperbPi
+
 sudo chmod +x ~/SuperbPi/mmll.py
 sudo chmod +x ~/SuperbPi/me7lconfig.py
 sudo chmod +x ~/SuperbPi/pylibme7.py
 
-#sudo cp raprec.service /lib/systemd/system
-#sudo chown root:root /lib/systemd/system/raprec.service
-#sudo chmod 0755 /lib/systemd/system/raprec.service
+sudo cp raprec.service /lib/systemd/system
+sudo chown root:root /lib/systemd/system/raprec.service
+sudo chmod 0755 /lib/systemd/system/raprec.service
 sudo systemctl daemon-reload
-#sudo systemctl enable raprec
+sudo systemctl enable raprec
 
 sudo chown -R www-data:www-data /var/www/html
 
