a081f40fd9c42efbc55b7d0144a34d068e5f009c
joachimth <46874512+joachimth@users.noreply.github.com>
2019-04-15 21:54:41 +0200

    Update localpi.sql

--- localpi.sql
+++ localpi.sql
@@ -1,7 +1,7 @@
-DROP DATABASE roadapplepi;
-CREATE DATABASE roadapplepi;
-GRANT ALL PRIVILEGES ON roadapplepi.* TO 'roadapplepi'@'%' IDENTIFIED BY 'roadapplepi';
-USE roadapplepi;
+DROP DATABASE SuperbPi;
+CREATE DATABASE SuperbPi;
+GRANT ALL PRIVILEGES ON SuperbPi.* TO 'SuperbPi'@'%' IDENTIFIED BY 'SuperbPi';
+USE SuperbPi;
 CREATE TABLE env (id int NOT NULL auto_increment, name text NOT NULL, value text NOT NULL, PRIMARY KEY(id));
 CREATE TABLE obdCodes (id int NOT NULL auto_increment, timestamp bigint NOT NULL, codes varchar(452) NOT NULL, PRIMARY KEY(id));
 CREATE TABLE obdLog(id bigint unsigned NOT NULL auto_increment, timestamp bigint not null, mode int not null, pid int not null, value varchar(100) not null, PRIMARY KEY(id));
